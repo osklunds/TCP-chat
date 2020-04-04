0a4f198e927c6547afd1660dc1d1524f58d4a7d0
Oskar Lundström <21679735+osklunds@users.noreply.github.com>
2020-04-04 08:14:04 +0200

    Handle incoming connections and print their data.

--- main.c
+++ main.c
@@ -6,16 +6,82 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <sys/select.h>
 
 #define LISTEN_BACKLOG 5
+#define MAX_NUM_CONNECTIONS 10
+#define RECV_BUF_SIZE 128
 
 
+void run_server_program(int port);
 int setup_server_listen_socket(int port);
 
 int main() {
     printf("Hello world\n");
 
-    int fd = setup_server_listen_socket(12345);
+    run_server_program(12345);
+}
+
+void run_server_program(int port) {
+    int listen_fd = setup_server_listen_socket(port);
+
+    int connections[MAX_NUM_CONNECTIONS];
+    int number_of_connections = 0;
+
+    while (1) {
+        fd_set fds;
+        FD_ZERO(&fds);
+        FD_SET(listen_fd, &fds);
+        int max_fd = listen_fd;
+
+        // Add all connections to fds
+        for (int i = 0; i < number_of_connections; i++) {
+            int current_fd = connections[i];
+            FD_SET(current_fd, &fds);
+            if (max_fd < current_fd) {
+                max_fd = current_fd;
+            }
+        }
+
+        if (select(max_fd + 1, &fds, NULL, NULL, NULL) < 0) {
+            perror("select() failed.");
+            exit(1);
+        }
+
+        // Accept a new client
+        if (FD_ISSET(listen_fd, &fds) && 
+            number_of_connections < MAX_NUM_CONNECTIONS) {
+            int client_fd = accept(listen_fd, NULL, 0);
+            connections[number_of_connections] = client_fd;
+            number_of_connections++;
+
+            printf("Accepted a client\n");
+        }
+
+        // Handle received data
+        for (int i = 0; i < number_of_connections; i++) {
+            int current_fd = connections[i];
+
+            if (FD_ISSET(current_fd, &fds)) {
+                char data[RECV_BUF_SIZE];
+
+                int len = recv(current_fd, data, RECV_BUF_SIZE, 0);
+
+                if (len < 0) {
+                    perror("Error with recv().");
+                    exit(1);
+                }
+
+                if (len == 0) {
+                    // TODO: Close connection
+                }
+
+                data[len] = 0;
+
+                printf("%s\n", data);
+            }
+        }
+    }
 }
 
 int setup_server_listen_socket(int port) {

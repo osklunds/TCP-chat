b5e0c1ec604fa9fabc5bb35cf04b20fad30d3876
Oskar Lundström <21679735+osklunds@users.noreply.github.com>
2020-04-05 08:02:55 +0200

    Add port command line argument.

--- main.c
+++ main.c
@@ -1,11 +1,20 @@
 
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "server.h"
 
 
-int main() {
-    printf("Hello world\n");
+int main(int argc, char *argv[]) {
+    if (argc == 2) {
+        char *port_string = argv[1];
+        int port = strtol(port_string, NULL, 10);
 
-    run_server_program(12345);
+        run_server_program(port);
+    } else if (argc == 3) {
+
+    } else {
+        printf("Invalid number of arguments.\n");
+        exit(1);
+    }
 }
